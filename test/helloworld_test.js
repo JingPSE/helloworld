4bc1ea4a3aad5c8ae3eeb848d1c169381d161ab9
JingPSE <39203659+JingPSE@users.noreply.github.com>
2018-05-29 22:06:51 -0700

    Update helloworld_test.js

--- test/helloworld_test.js
+++ test/helloworld_test.js
@@ -1,7 +1,7 @@
 var Browser = require('zombie')
 var assert = require('assert')
-var app = require('../helloworld').app
-var server = require('../helloworld').server
+var app = require('../helloworld')
+
 
 describe('main page', function() { 
   before(function() {
@@ -10,10 +10,6 @@ describe('main page', function() {
   before(function(done) {
     this.browser.visit('/', done)
   })
-  after(function (done) {
-        server.close();
-        done();
-   })
   it('should say hello world', function() { 
     assert.ok(this.browser.success)
     assert.equal(this.browser.text(), "Hello World")
